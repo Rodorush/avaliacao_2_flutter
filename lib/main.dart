1e5d4a9d77013afa3a167f2cf03b090f32f66efd
Rodolfo Pereira de Andrade <rodolfop.andrade@gmail.com>
2024-04-08 23:10:32 -0300

    Ajustado layout
    Ajustado Layout para fidelidade da tela proposta no exercício do PDF em anexo.

--- lib/main.dart
+++ lib/main.dart
@@ -32,39 +32,68 @@ class Home extends StatelessWidget {
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 20),
+              const Text('NOME:'),
+              const SizedBox(height: 7),
               const TextField(
                 decoration: InputDecoration(
-                  labelText: 'NOME',
-                  hintText: 'Digite o nome',
+                  border: OutlineInputBorder(),
+                  labelText: 'Digite o nome'
                 ),
               ),
+              const SizedBox(height: 28),
+              const Text('ENDEREÇO:'),
+              const SizedBox(height: 7),
               const TextField(
                 decoration: InputDecoration(
-                  labelText: 'ENDEREÇO',
-                  hintText: 'Digite o endereço',
+                  border: OutlineInputBorder(),
+                  labelText: 'Digite o endereço',
                 ),
               ),
+              const SizedBox(height: 28),
+              const Text('eMAIL:'),
+              const SizedBox(height: 7),
               const TextField(
                 decoration: InputDecoration(
-                  labelText: 'E-MAIL',
-                  hintText: 'Digite o e-mail',
+                  border: OutlineInputBorder(),
+                  labelText: 'Digite o email',
                 ),
               ),
               const SizedBox(height: 20),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
                   ElevatedButton(
                     onPressed: () {
                       _mostrarDialogo(context, 'Cancelar');
                     },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                      shape: MaterialStateProperty.all<OutlinedBorder>(
+                        const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(4.0),
+                          )
+                        )
+                      )
+                    ),
                     child: const Text('CANCELAR',
                         style: TextStyle(color: Colors.white)),
                   ),
+                  const SizedBox(width: 20),
                   ElevatedButton(
                     onPressed: () {
                       _mostrarDialogo(context, 'Salvar');
                     },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                            const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(4.0),
+                                )
+                            )
+                        )
+                    ),
                     child: const Text('SALVAR',
                         style: TextStyle(color: Colors.white)),
                   ),
