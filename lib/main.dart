5c4aaa98b52f1de5addb3c9c18fbda97164d4e72
Rodolfo Pereira de Andrade <rodolfop.andrade@gmail.com>
2024-04-08 22:44:17 -0300

    Implementando caixa de diálogo
    Necessário criar um Widget Home para enviar um contexto abaixo de MaterialApp para o ShowDialog. Se o contexto fica acima então é gerado um erro.

--- lib/main.dart
+++ lib/main.dart
@@ -7,62 +7,93 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Exercício Montagem GUI",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Exercício Montagem GUI'),
-          backgroundColor: Colors.lightBlue,
-          foregroundColor: Colors.white,
-        ),
-        body: const SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'TELA DE CADASTRO',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Exercício Montagem GUI'),
+        backgroundColor: Colors.lightBlue,
+        foregroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'TELA DE CADASTRO',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'NOME',
+                  hintText: 'Digite o nome',
                 ),
-                SizedBox(height: 20),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'NOME',
-                    hintText: 'Digite o nome',
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'ENDEREÇO',
+                  hintText: 'Digite o endereço',
+                ),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'E-MAIL',
+                  hintText: 'Digite o e-mail',
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      _mostrarDialogo(context, 'Cancelar');
+                    },
+                    child: const Text('CANCELAR',
+                        style: TextStyle(color: Colors.white)),
                   ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'ENDEREÇO',
-                    hintText: 'Digite o endereço',
+                  ElevatedButton(
+                    onPressed: () {
+                      _mostrarDialogo(context, 'Salvar');
+                    },
+                    child: const Text('SALVAR',
+                        style: TextStyle(color: Colors.white)),
                   ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'E-MAIL',
-                    hintText: 'Digite o e-mail',
-                  ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: null,
-                      child:
-                      Text('CANCELAR', style: TextStyle(color: Colors.white)),
-                    ),
-                    ElevatedButton(
-                      onPressed: null,
-                      child:
-                      Text('SALVAR', style: TextStyle(color: Colors.white)),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  void _mostrarDialogo(BuildContext context, String acao) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Caixa de Diálogo'),
+          content: Text('Pressionado botão $acao.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Fechar'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
